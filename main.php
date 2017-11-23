8075328021ddb5bf5d46217203b2e19f543322f5
peter279k <peter279k@gmail.com>
2017-11-23 22:10:45 +0800

    add the for loop for next page

--- main.php
+++ main.php
@@ -27,49 +27,51 @@ $place = urlencode('新竹');
 $page = 1;
 $endPage = 384;
 
-$resource = str_replace(['{page}', '{place}'], [$page, $place], $resource);
+for(;$page<=$endPage;$page++) {
+    $resource = str_replace(['{page}', '{place}'], [$page, $place], $resource);
 
-$request = new CrawlRequest($resource);
-$body = $request->_request(15);
-
-$foodLink = new CrawlFoodLink();
-$link = $foodLink->shouldCrawl($body);
-
-foreach($link as $val) {
-    $valArray = explode('-', $val);
-    $val = $valArray[0];
-
-    if($val == 'not') {
-        continue;
-    }
-
-    $request->setResource($val);
+    $request = new CrawlRequest($resource);
     $body = $request->_request(15);
 
-    $shopStatus = new CrawlShopStatus();
-    $status = $shopStatus->shouldCrawl($body);
+    $foodLink = new CrawlFoodLink();
+    $link = $foodLink->shouldCrawl($body);
 
-    // if shop status is not the empty string, the shop has been closed.
-    if($status != '') {
-        continue;
+    foreach($link as $val) {
+        $valArray = explode('-', $val);
+        $val = $valArray[0];
+
+        if($val == 'not') {
+            continue;
+        }
+
+        $request->setResource($val);
+        $body = $request->_request(15);
+
+        $shopStatus = new CrawlShopStatus();
+        $status = $shopStatus->shouldCrawl($body);
+
+        // if shop status is not the empty string, the shop has been closed.
+        if($status != '') {
+            continue;
+        }
+
+        $foodAddress = new CrawlAddress();
+        //$foodBusinessHours = new CrawlBusinessHours();
+        $foodPhoneNumber = new CrawlPhoneNumber();
+        $foodRate = new CrawlRate();
+        $foodShopName = new CrawlShopName();
+        $foodImage = new CrawlMapImage();
+
+        $address = $foodAddress->shouldCrawl($body);
+        //$businessHours = $foodBusinessHours->shouldCrawl($body);
+        $phoneNumber = $foodPhoneNumber->shouldCrawl($body);
+        $rate = $foodRate->shouldCrawl($body);
+        $shopName = $foodShopName->shouldCrawl($body);
+        $image = $foodImage->shouldCrawl($body);
+
+        $foodInfoStr = implode([$address, $phoneNumber, $rate, $shopName, $image], ',');
+        file_put_contents($filePath, $foodInfoStr.PHP_EOL, FILE_APPEND);
+
+        sleep(rand(1, 10));
     }
-
-    $foodAddress = new CrawlAddress();
-    //$foodBusinessHours = new CrawlBusinessHours();
-    $foodPhoneNumber = new CrawlPhoneNumber();
-    $foodRate = new CrawlRate();
-    $foodShopName = new CrawlShopName();
-    $foodImage = new CrawlMapImage();
-
-    $address = $foodAddress->shouldCrawl($body);
-    //$businessHours = $foodBusinessHours->shouldCrawl($body);
-    $phoneNumber = $foodPhoneNumber->shouldCrawl($body);
-    $rate = $foodRate->shouldCrawl($body);
-    $shopName = $foodShopName->shouldCrawl($body);
-    $image = $foodImage->shouldCrawl($body);
-
-    $foodInfoStr = implode([$address, $phoneNumber, $rate, $shopName, $image], ',');
-    file_put_contents($filePath, $foodInfoStr.PHP_EOL, FILE_APPEND);
-
-    sleep(rand(1, 10));
 }
